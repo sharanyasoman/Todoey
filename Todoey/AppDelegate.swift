00f5e52545fa9092b88eb4b9aebae18f5e4f4ad7
Sharanya Soman <ssoman@vmware.com>
2019-09-19 21:06:25 +0530

    Install RealmSwift pod

--- Todoey/AppDelegate.swift
+++ Todoey/AppDelegate.swift
@@ -7,7 +7,6 @@
 //
 
 import UIKit
-import CoreData
 import RealmSwift
 
 @UIApplicationMain
@@ -21,42 +20,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 //    User Defaults    print(NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).last! as String )
         print("Realm path:")
         print(Realm.Configuration.defaultConfiguration.fileURL as Any)
-
-        return true
-    }
-
-
-
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-        // Saves changes in the application's managed object context before the application terminates.
-        self.saveContext()
-    }
-    
-    // MARK: - Core Data stack
-    //NSpersistent container - SQLite DB basically
-    lazy var persistentContainer: NSPersistentContainer = {
-        let container = NSPersistentContainer(name: "DataModel")
-        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
-            if let error = error as NSError? {
-                fatalError("Unresolved error \(error), \(error.userInfo)")
-            }
-        })
-        return container
-    }()
-    
-    // MARK: - Core Data Saving support
-    
-    func saveContext () {
-        let context = persistentContainer.viewContext
-        if context.hasChanges {
-            do {
-                try context.save()
-            } catch {
-                let nserror = error as NSError
-                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
-            }
+        do{
+            _ = try Realm()
+        }catch{
+            print("Error initializing new realm, \(error)")
         }
+        return true
     }
 
 
